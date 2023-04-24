46402080fb4e0be828e772279dac3aa11fa32eec
Andrei Krug <andrei.krug.01tec@gmail.com>
2023-04-24 15:02:45 -0300

    alterando codigo

--- ArvoreAVL/src/InterfaceUsuario.java
+++ ArvoreAVL/src/InterfaceUsuario.java
@@ -3,34 +3,35 @@ import java.util.Scanner;
 public class InterfaceUsuario 
 {
 
-   public void mensagens(){
+    public void mensagens(){
         int valor = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite:\n 1 para inserir valor\n 2 para excluir valor\n 3 para acessar modos de impressão da arvore\n 4 para procurar valor na arvore\n 7 para sair\n");
-        valor = scanner.nextInt();
         Arvore a = new Arvore();
         while(valor != 7){
-            while(valor > 4 || valor < 1){
-            System.out.println("Digite:\n 1 para inserir valor\n 2 para excluir valor\n 3 para acessar modos de impressão da arvore\n 4 para procurar valor na arvore\n 7 para sair\n");
+            System.out.println("###Digite:\n 1 para inserir valor\n 2 para excluir valor\n 3 para acessar modos de impressão da arvore\n 4 para procurar valor na arvore\n 7 para sair\n -------------------------------------------------------");
             valor = scanner.nextInt();
-        }
+            while(valor > 4 || valor < 1)
+            {
+            System.out.println("###Digite:\n 1 para inserir valor\n 2 para excluir valor\n 3 para acessar modos de impressão da arvore\n 4 para procurar valor na arvore\n 7 para sair\n -------------------------------------------------------");
+            valor = scanner.nextInt();
+            }
         int input;
         if(valor == 1)
         {
-            System.out.println("Digite o valor que deseja inserir na árvore");
+            System.out.println("###Digite o valor que deseja inserir na árvore");
             input = scanner.nextInt();
             a.inserir(input);
         } else if(valor == 2){
-            System.out.println("Digite o valor que deseja excluir da árvore");
+            System.out.println("###Digite o valor que deseja excluir da árvore");
             input = scanner.nextInt();
             a.excluir(input, a.getRaiz());
         } else if(valor == 3){
             int selec;
-            System.out.println("Digite:\n 1 para ordem\n 2 para pre ordem\n 3 para pos ordem\n");
+            System.out.println("###Digite:\n 1 para ordem\n 2 para pre ordem\n 3 para pos ordem\n");
             selec = scanner.nextInt();
             while(selec > 3 || selec < 1)
             {
-                System.out.println("Digite:\n 1 para ordem\n 2 para pre ordem\n 3 para pos ordem\n");
+                System.out.println("###Digite:\n 1 para ordem\n 2 para pre ordem\n 3 para pos ordem\n");
                 selec = scanner.nextInt();
             }
             if(selec == 1)
@@ -44,7 +45,7 @@ public class InterfaceUsuario
             }
         } else if(valor == 4){
             int proc;
-            System.out.println("Digite um valor para procurar na árvore");
+            System.out.println("###Digite um valor para procurar na árvore");
             proc = scanner.nextInt();
             a.procurar(proc, a.getRaiz());
         }
--- ArvoreAVL/src/PrincipalArvore.java
+++ ArvoreAVL/src/PrincipalArvore.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class PrincipalArvore {
 	public static void main(String[] args) {
-		InterfaceUsuario i = new InterfaceUsuario();
-		i.mensagens();
+ 
 	}
 }
